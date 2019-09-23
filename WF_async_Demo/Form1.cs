8c9cad82ba29f79f96082cf652431893a2feacf3
Jakub Syrek <jaqb.syrek@gmail.com>
2019-09-24 00:56:10 +0200

    1

--- WF_async_Demo/Form1.cs
+++ WF_async_Demo/Form1.cs
@@ -10,11 +10,35 @@ using System.Windows.Forms;
 
 namespace WF_async_Demo
 {
+
     public partial class Form1 : Form
     {
+        public static string outp = "";
+
         public Form1()
-        {
+        {     
+
             InitializeComponent();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            while (textBox1.Text == "")
+            {
+                if (outp != "") { textBox1.Text = outp; break; }
+                if (backgroundWorker1.IsBusy != true)
+                    backgroundWorker1.RunWorkerAsync(2000);
+            }         
+        }
+
+        private void  backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            Pinger pinger = new Pinger();
+            pinger.PingOnet().Wait();
+            MessageBox.Show(pinger.Output);
+            outp = pinger.Output;
+           
         }
     }
 }
---
+++ WF_async_Demo/Pinger.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WF_async_Demo
+{
+   
+
+    public class Pinger
+    {
+        private string _output1;
+
+        public string Output            
+        { 
+            get { return _output1; }
+            set { _output1 = value; }
+        }
+
+        
+        public async Task<string> PingOnet()
+        {
+            Ping myPing = new Ping();           
+            
+            var _replied = await myPing.SendPingAsync("213.180.141.140", 1000);           
+             _output1 = $"{ _replied.Address}/{_replied.RoundtripTime}";
+            //MessageBox.Show(_output);
+            return _output1;
+        }
+
+      
+    }
+}
